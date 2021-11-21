e374cfd42bb07c93e5b2d1eedcf1b3e098a63651
Arsalan Radhu <arsalan.radhu@dcmail.ca>
2021-11-21 17:50:03 -0500

    Added some properties

--- src/ca/durhamcollege/WorkTicket.java
+++ src/ca/durhamcollege/WorkTicket.java
@@ -1,6 +1,7 @@
 package ca.durhamcollege;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class WorkTicket
 {
@@ -14,5 +15,29 @@ public class WorkTicket
     public int getNumber()
     {
         return number;
+    }
+
+    public void setNumber()
+    {
+        //Declarations
+        Scanner keyboard = new Scanner(System.in);
+        boolean isValid = true;
+        int number = 0;
+
+        while(isValid)
+        {
+            System.out.print("\nPlease enter a ticket number: ");
+            number = keyboard.nextInt();
+            if(number <= 0)
+            {
+                throw new IllegalArgumentException("ERROR! only positive numbers");
+            }
+            else
+            {
+                this.number = number;
+                isValid = false;
+            }
+        }
+
     }
 }
