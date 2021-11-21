7b79c77facaa6b06f30f81bc1b8a0adac148809c
Arsalan Radhu <arsalan.radhu@dcmail.ca>
2021-11-21 18:08:17 -0500

    Added some Client ID property

--- src/ca/durhamcollege/WorkTicket.java
+++ src/ca/durhamcollege/WorkTicket.java
@@ -17,27 +17,57 @@ public class WorkTicket
         return number;
     }
 
-    public void setNumber()
+    public void setTicketNumber()
     {
         //Declarations
         Scanner keyboard = new Scanner(System.in);
         boolean isValid = true;
-        int number = 0;
+        int ticketNumber = 0;
 
         while(isValid)
         {
-            System.out.print("\nPlease enter a ticket number: ");
-            number = keyboard.nextInt();
-            if(number <= 0)
+            System.out.print("\nEnter a ticket number: ");
+            ticketNumber = keyboard.nextInt();
+            if(ticketNumber <= 0)
             {
-                throw new IllegalArgumentException("ERROR! only positive numbers");
+                throw new IllegalArgumentException("Please enter positive numbers only.");
             }
             else
             {
-                this.number = number;
+                this.number = ticketNumber;
                 isValid = false;
             }
         }
 
     }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public void setId()
+    {
+        //Declarations
+        Scanner keyboard = new Scanner(System.in);
+        boolean isValid = true;
+        String id = null;
+        while(isValid)
+        {
+            System.out.print("\nEnter a custom ID: ");
+            id = keyboard.nextLine();
+            if (id.length() <= 0)
+            {
+                throw new IllegalArgumentException("Input should not be empty.");
+            }
+            else
+            {
+                this.id = id;
+                isValid = false;
+            }
+        }
+
+
+    }
+
 }
