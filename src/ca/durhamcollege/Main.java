83f9ea0c203b03e4332bf7cb34a47045efd7290d
Arsalan Radhu <arsalan.radhu@dcmail.ca>
2021-11-21 19:58:09 -0500

    Done with the main.java

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,9 +1,39 @@
 package ca.durhamcollege;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        //Declarations
+        //Variables
+        Scanner keyboard = new Scanner(System.in);
+        boolean isValid = true;
+        WorkTicket ticket = new WorkTicket();
+
+        do
+        {
+            try
+            {
+                ticket.SetWorkTicket();
+                System.out.println(ticket.toString());
+                isValid = false;
+            }
+            catch(IllegalArgumentException iae)
+            {
+                System.out.printf("\n INVALID: %s", iae.toString());
+                keyboard.nextLine();
+                isValid = true;
+            }
+            catch(InputMismatchException ime)
+            {
+                System.out.print("\nINVALID: Your input was incorrect, try again.");
+                keyboard.nextLine();
+                isValid = true;
+            }
+        } while (isValid);
 
     }
 }
